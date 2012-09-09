eab026dea1d13d7ec5169de10160139be641d6dd
Davids Murafa <davidsmurafa@gmail.com>
2012-09-10 00:16:06 +0300

    sagatave

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php
+
+$db = mysql_connect("localhost","davx","Melnis92");
+if (!$db)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+  
+$db_selected = mysql_select_db('digdig', $db);
+
+?>
